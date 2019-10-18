76449f183d32dd420a760b1a92e38180d5290eda
Jewls Krueger <jewls@Jewlss-MacBook-Pro.local>
2019-10-18 11:13:43 -0700

    modal now display's that pokemon's stats from API pull

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,30 @@
 $(()=>{
 
+  let $pokemonModal = $('.modal')
 
+  //click function for choosing your pokemon
   $('.pokemon-picker').on('click', (event)=> {
     let $pokemonName = $(event.currentTarget).children('img').attr('id')
-    let $pokemonModal = $(event.currentTarget).children('div')
+    let $pokemonModal = $('.modal');
+
+    //toggle seeing the pokemon's information or not.
     $pokemonModal.toggle();
+
+    //api pull to get pokemon information for only the pokemon that was clicked
     $.ajax ({
       url:'https://pokeapi.co/api/v2/pokemon/'+$pokemonName,
     }).then(
       (data)=> {
-        console.log("This Pokemon's type is: "+ data.name);
-        console.log("This Pokemon has " + data.base_experience + " XP");
+        let $pokemonName = data.name
+        let $pokemonXP = data.base_experience
+        console.log("This Pokemon's type is: "+ $pokemonName);
+        console.log("This Pokemon has " + $pokemonXP + " XP");
+
+        $('.poke-type').text($pokemonName)
+        $('.poke-xp').text($pokemonXP)
       }
     )
-  })
+
+  }) //closing tag for click function
 
 })
