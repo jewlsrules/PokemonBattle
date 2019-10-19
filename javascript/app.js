db87d7ffe887cd6c638c4dc600399729e3692aba
Jewls Krueger <jewls@Jewlss-MBP.T-mobile.com>
2019-10-19 14:19:52 -0700

    updated some game mechanics for new data structure

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,8 @@
 //players information
  const player = {
    pokemon: [{
-     name: "charmander",
+     name: "no pokemon yet",
+     pokedexNum: 0,
      xp: 0,
      attacks:
        []
@@ -9,31 +10,31 @@
    bank: 0,
    wins: 0,
    losses: 0,
+   turn: true,
  };
 
+//on load
 $(()=>{
 
+  ///////////////////////
+  //Variable Set Up
+  ///////////////////////
+
   //basic layout set up
-  const $choosePokemon = $('.choose-pokemon')
-  let $clickToPick = $('.click-to-pick')
-  let $playersPokemonDiv = $('.players-pokemon')
-  let $battleStartButton = $('<div>').text('Start Battle').attr('id', 'start-battle')
-  let $attackButton1 = $('<div>').addClass('clickable')
-  let $attackButton2 = $('<div>').addClass('clickable')
-  let renderInBattleAttacks = () => {
-      $('.click-area').append($attackButton1.text(moveArray[0]).addClass('pokemon-move'));
-      $('.click-area').append($attackButton2.text(moveArray[1]).addClass('pokemon-move'))
-  }
+  const $choosePokemon = $('.choose-pokemon');
+  let $clickToPick = $('.click-to-pick');
+  let $playersPokemonDiv = $('.players-pokemon');
+  let $battleStartButton = $('<div>').text('Start Battle').attr('id', 'start-battle');
+  let $attackButton1 = $('<div>').addClass('clickable');
+  let $attackButton2 = $('<div>').addClass('clickable');
 
   //game information & storage
-  let playersTurn = true
-
-  let playersCurrentPokemon
+  let playersCurrentPokemon;
   let $playersPokemonImgSrc;
   const moveArray = [];
-  let pokemonXP
-  let moveApiUrl = " "
-  let playersBank = 0;
+  let pokemonXP;
+  let moveApiUrl = " ";
+  let tempPokemonConfirmName;
 
   //opponent information
   let possOpponents = [{
@@ -47,11 +48,11 @@ $(()=>{
     levle: 2,
     reward: 35,
     xp: 60
-  }]
-  let currentOpponentIndex = 0
-  let opponentPokemon = possOpponents[currentOpponentIndex].name
+  }];
+  let currentOpponentIndex = 0;
+  let opponentPokemon = possOpponents[currentOpponentIndex].name;
   const oppMoveArray = [];
-  let opponentXP = possOpponents[currentOpponentIndex].xp
+  let opponentXP = possOpponents[currentOpponentIndex].xp;
 
 
   ////////////////////////////
@@ -91,8 +92,10 @@ $(()=>{
       url:'https://pokeapi.co/api/v2/pokemon/'+ $chosenPokemonName,
     }).then(
       (data)=> {
+        //update the player's object with the pokemon's name and base xp
         player.pokemon[0].name = data.name;
         player.pokemon[0].xp = data.base_experience;
+        player.pokemon[0].pokedexNum = data.id;
         //loop through moves array and find the ones that have version group name "red-blue" && starter level 1
             for(let i = 0; i<data.moves.length; i++){
               let array1 = data.moves[i].version_group_details
@@ -101,31 +104,37 @@ $(()=>{
                   //only find moves from red & blue original games
                   if((array1[j].version_group.name === "red-blue") && (array1[j].level_learned_at === 1)) {
                     let moveName = data.moves[i].move.name;
-                    // let moveForAttackArray = {attackName: data.moves[i].move.name};
-                    // // this will push the name of the attack into the attack array
-                    // player.pokemon[0].attacks.push(moveForAttackArray)
+                    //this function will find the attack URL and eventually push the moves into the player's pokemon's array
                     findAttackID(moveName)
                   };
                 }
               } //end of the for loop
-        console.log(player);
+        // console.log(player);
     })
-    // when a player selects a pokemon, it should hide the picker area
-    $choosePokemon.hide();
+    //save the url of the chosen pokemon to use in the battle area
+    $playersPokemonImgSrc = $(event.currentTarget).parent().children('.poke-picture').children('img').attr('src')
+    //confirm the pokemon choice to allow time for API to populate player's object with pokemon information'
+    confirmPokemon();
   })
 
+//function to find the information about the moves for a pokemon
   const findAttackID = (attack) => {
-    console.log('starting findAttackID function, looking for '+attack);
+    // console.log('starting findAttackID function, looking for '+attack);
     $.ajax ({
+      //this will pull up all attacks, not limited to an amount per page
       url:'https://pokeapi.co/api/v2/move/?offset=0&limit=800'
        }).then(
          (data)=> {
+           //variable for the length of the results array
            let resultsLength = data.results.length
-           console.log('find attack ID returned this many results: '+resultsLength);
+           // console.log('find attack ID returned this many results: '+resultsLength);
+           //loop through each attack and find only the ones we're looking for
            for(let i=0;i<resultsLength;i++){
              if (data.results[i].name === attack) {
+               //get the url of the attacks that we're looking for and store them in a variable
                moveApiUrl = data.results[i].url;
-               console.log('url for '+attack+'found: '+moveApiUrl);
+               // console.log('url for '+attack+'found: '+moveApiUrl);
+               //the next function will pull up an api request using the found URL to get the stats for that particular attack
                $getAttackStats(moveApiUrl);
                return;
              }
@@ -133,6 +142,7 @@ $(()=>{
       })
   }
 
+  //this function goes to the attack's API page and pulls information from that and stores it in the user's pokemon array
   const $getAttackStats = (url) => {
     $.ajax ({
       //we get the url from the attack button click listener
@@ -141,55 +151,56 @@ $(()=>{
       (data) => {
           //get the attack power base & turn it into an integer
           let attackInt = parseInt(data.power)
-          console.log('finding the data for '+ data.name + '. power is: ' + data.power);
+          // console.log('finding the data for '+ data.name + '. power is: ' + data.power);
           //push attack information into player's pokemon's attack array
           player.pokemon[0].attacks.push({attackName: data.name, power: attackInt})
       }
     )
   }
 
-    //get the url of the current pokemon's photo to display in the battle area div
-  //   $playersPokemonImgSrc = $(event.currentTarget).parent().children('.poke-picture').children('img').attr('src');
-  //   createPlayersPokemonArea();
-  //   createOpponentPokemonArea();
-  //   initializeBattle();
-  // })
+  //confirm pokemon choice - we need this buffer in here to ensure that the API has pulled all the information into the players array before startng the first battle and rendering information
+  const confirmPokemon = () => {
+    //hide the div with the starter pokemon in it
+    $choosePokemon.hide();
+    //create a new div that the player can click on to confirm their choice
+    ///*****NEEDS UPDATING FROM HARDCODED CHARMANDER*****
+    let $confirmDiv = $('<div>').text('click here to confirm you want charmander').on('click', startFirstBattle).attr('id','confirm')
+    //put this new div in the main area
+    /////*****NEEDS STYLING!!******//
+    $('.container').append($confirmDiv)
+  }
 
+  /////////////////////////////////
+  //FIRST BATTLE LOGIC
+  /////////////////////////////////
+
+  //initialize first battle Function
+  const startFirstBattle = () => {
+    //hide the confirmation div, we no longer need it after the player has confirmed
+    $('#confirm').hide();
+    //create the div that shows the players pokemon information
+    createPlayersPokemonArea();
+    //create the div that shows the opponent's pokemon information
+    createOpponentPokemonArea();
+    initializeBattle();
+  };
+
+  //this function will create the player's pokemon area and get data from the player object
   const createPlayersPokemonArea = () => {
     //show the div that most of the game action will happen in.
     $('.battle-play').show().css('display', 'flex');
     //show the photo of the pokemon that was chosen
     $playersPokemonDiv.append($('<img>').attr('src', $playersPokemonImgSrc).addClass('players-pokemon-photo'))
     //show the name of the current pokemon
-    $playersPokemonDiv.append($('<div>').text(playersCurrentPokemon).addClass('current-pokemon-name'))
-    //pull information about the players current pokemon
-    $.ajax ({
-      url:'https://pokeapi.co/api/v2/pokemon/'+playersCurrentPokemon,
-    }).then(
-      (data)=> {
-        // //loop through moves array and find the ones that have version group name "red-blue" && starter level 1
-        // for(let i = 0; i<data.moves.length; i++){
-        //   let array1 = data.moves[i].version_group_details
-        //   let versionLength = data.moves[i].version_group_details.length;
-        //     for(let j = 0; j<versionLength; j++){
-        //       if((array1[j].version_group.name === "red-blue") && (array1[j].level_learned_at === 1)) {
-        //         let move = data.moves[i].move.name;
-        //         moveArray.push(move)
-        //       };
-        //     }
-        //   } //end of the for loop
-          //go through and list the attacks for this pokemon
-          for(let k = 0; k<moveArray.length; k++){
-            $('.players-pokemon').append($('<div>').text(moveArray[k]).addClass('pokemon-move'))
-          } //end of this for loop
-          //display the starting XP for the pokemon
-          let pokemonXP = data.base_experience;
-          let displayXP = $('<div>').text("XP: "+ pokemonXP).addClass('xp-stats')
-          $('.players-pokemon').append(displayXP)
-      })
+    console.log('player\'s current pokemon is '+ player.pokemon[0].name);
+    $playersPokemonDiv.append($('<div>').text(player.pokemon[0].name).addClass('current-pokemon-name'))
+    //display the pokemon's xp
+    $playersPokemonDiv.append($('<div>').text('XP: '+ player.pokemon[0].xp).addClass('xp-stats'))
   }; //end of the createPlayersPokemonArea function
 
+  //this function will create the opponent's area with all the opponent pokemon's information
   const createOpponentPokemonArea = () => {
+    //hardcoded for now, needs to be updated eventually
     let caterpiePicture = $('<img>').attr('src', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png').addClass('players-pokemon-photo')
     $('.opponent-area').append(caterpiePicture)
     $('.opponent-area').append($('<div>').text("Caterpie").addClass('current-pokemon-name'))
@@ -230,14 +241,21 @@ $(()=>{
  //click listener for battle start button
  $battleStartButton.on('click', ()=>{
    $battleStartButton.hide();
-   let $playerGoesFirstDiv = $('<div>').text('Go '+playersCurrentPokemon+"!")
-   let $pickAnAttack = $('<div>').text('What attack do you want '+playersCurrentPokemon+' to do?')
+   let $playerGoesFirstDiv = $('<div>').text('Go '+player.pokemon[0].name+"!")
+   let $pickAnAttack = $('<div>').text('What attack do you want '+player.pokemon[0].name+' to do?')
 
    $('.click-area').append($playerGoesFirstDiv)
    $('.click-area').append($pickAnAttack)
 
    renderInBattleAttacks();
  })// end of battle start button click listener
+
+//render the buttons for the battle
+ let renderInBattleAttacks = () => {
+   //this could get changed into a for loop...
+     $('.click-area').append($attackButton1.text(player.pokemon[0].attacks[0].attackName).addClass('pokemon-move'));
+     $('.click-area').append($attackButton2.text(player.pokemon[0].attacks[1].attackName).addClass('pokemon-move'))
+ };//end of renderInBattleAttacks function
 
  //player's pokemon attack chosen
   $attackButton1.on('click', (event)=> {
@@ -254,9 +272,9 @@ $(()=>{
               console.log(moveApiUrl);
               $getAttackStats(moveApiUrl);
               return;
-            }
-          }
-     })
+            }//end of if statement
+          }//end of for loop
+     })//end of ajax call
    })//end of attack button 1 function
 
    //player's pokemon attack chosen
@@ -303,32 +321,6 @@ $(()=>{
       updateOppXP();
    }
  }
-
- // //this will get the attack power from the API and reduce the opponent's XP by that much.
- // //this is nested inside of the click function for when a player chooses an attack
- // const $getAttackStats = (url) => {
- //   $.ajax ({
- //     //we get the url from the attack button click listener
- //     url: url
- //   }).then(
- //     (data) => {
- //       if(data.power){
- //         //get the attack power base & turn it into an integer
- //         let attackInt = parseInt(data.power)
- //         //turns the current xp into an integer
- //         let opponentXpInt = parseInt(possOpponents[currentOpponentIndex].xp)
- //         //get the resulting xp after the attack and update the object
- //         possOpponents[currentOpponentIndex].xp = opponentXpInt - attackInt
- //         opponentXP = possOpponents[currentOpponentIndex].xp
- //         //function to update the display of the opponent's XP.
- //         checkForOppDefeat();
- //       } else {
- //         //if the attack has null power, it will not take away from opponent's xp
- //         console.log('The attack had no effect!');
- //       }
- //     }
- //   )
- // }
 
 //player wins function
   const playerWins = () => {
